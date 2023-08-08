ffcf16bdc2658b0765fa2a6d07363481d81035f1
Girish Bhosale <87483444+GirishBhosale@users.noreply.github.com>
2023-08-08 23:25:37 +0530

    02_dataTypes.js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS Code as newer version
+
+
+// alert(3 + 3) // we are using node js environment, not browser
+
+console.log(3 + 3); //code readibilty should be high
+
+console.log("Girish")
+
+
+let name = "Girish"
+let age = 18
+let isLoggedIn = false
+let state; // ; undefined because value is not given
+
+
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// above this are primitive data types
+
+
+
+// object 
+
+console.log(typeof null); //shows object,  null is object
+console.log(typeof undefined); // shows undefines
