2f36d51328a84f51d26013df7769f1c417565bdb
Girish Bhosale <87483444+GirishBhosale@users.noreply.github.com>
2023-08-10 23:57:15 +0530

    datatypes-summary.js

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,43 @@
+// primitive
+
+// 7 types: string, number, boolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+
+// const bigNumber = 165623236551316623n
+
+
+
+// Reference type(Non primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman", "naagraj", "doha"];
+let myObj = {
+    name: "Girish"
+
+}
+
+function myfunction() {
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+
+
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
