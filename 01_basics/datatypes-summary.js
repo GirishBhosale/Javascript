952e1d8866b552359eac9d3708b36006eb8cc5cb
Girish Bhosale <87483444+GirishBhosale@users.noreply.github.com>
2023-08-11 22:38:29 +0530

    datatypes-summary.js

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -41,3 +41,30 @@ console.log(typeof anotherId);
 
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++++++++++++++++++++++
+
+
+// stack(primitve), heap(non primitve)  memory
+
+let myYoutubename = "Girish"
+
+let anothername = myYoutubename
+anothername = "cactus"
+
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "gb@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@goofle.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
