452317d356b85cc90ef970f928a0c23100108115
Girish Bhosale <87483444+GirishBhosale@users.noreply.github.com>
2023-08-13 17:37:20 +0530

    06_nums_and_math.js

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -4,6 +4,7 @@ const score = 400
 const balance = new Number(100)
 // console.log(balance);
 
+
 // console.log(balance.toString().length);
 // console.log(balance.toFixed(1));
 
@@ -11,16 +12,19 @@ const otherNumber = 123.8966
 
 // console.log(otherNumber.toPrecision(4));
 
-const hundreds = 1000000
-// console.log(hundreds.toLocaleString('en-IN'));
 
-// +++++++++++++ Maths +++++++++++++++++++++++++++++
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+
+
+// +++++++++++++++ maths +++++++++++++++++++++++++++
 
 // console.log(Math);
+
 // console.log(Math.abs(-4));
 // console.log(Math.round(4.6));
 // console.log(Math.ceil(4.2));
-// console.log(Math.floor(4.9));
+// console.log(Math.floor(4.2));
 // console.log(Math.min(4, 3, 6, 8));
 // console.log(Math.max(4, 3, 6, 8));
 
@@ -32,3 +36,9 @@ const min = 10
 const max = 20
 
 console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+
+
