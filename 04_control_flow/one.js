c2ee2e8009ff253b2e00a53333f9a614f8e68f6f
Girish Bhosale <87483444+GirishBhosale@users.noreply.github.com>
2023-08-23 22:46:33 +0530

    Create one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,54 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+
+// if(temperature === 40){
+//  console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("executed");
+
+// <, >, <=. >=, ==, !=, ===, !==
+
+// const score = 200
+
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// console.log(`User power: ${power}`);
+
+
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test");
+// console.log("test2");
+
+// if (balance < 500){
+//     console.log("less thane");
+// }else if (balance < 750){
+//     console.log("less than 750");
+// }
+// else if (balance < 900){
+//     console.log("less than 1200");
+// }
+// else {
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitcard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitcard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+}
