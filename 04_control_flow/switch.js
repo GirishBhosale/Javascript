6e1675e38f2b62e2cd758816d2ded68019f046f5
Girish Bhosale <87483444+GirishBhosale@users.noreply.github.com>
2023-08-24 22:48:12 +0530

    switch.js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "mar"
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case "mar":
+        console.log("mar");
+        break;
+    case 4:
+        console.log("apr");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
